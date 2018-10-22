645665070c4115e70c33811248b085889b037d32
Nick Dean <klick5000@gmail.com>
2018-10-22 16:32:11 -0600

    Initial setup

---
+++ index.js
@@ -0,0 +1,15 @@
+//Get Candidate Github list
+//-setup query
+//-put url together (FormatQueryParams)
+//-add API key to header
+//-FETCH(url, options)
+// .then
+// .then
+// .catch
+
+//Display results
+
+//Watch for event submit on button
+//(send info to function)
+
+// Jquery watch form
