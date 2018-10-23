8a388a067056f5a6f40cbe1410b03d090586432c
Nick Dean <klick5000@gmail.com>
2018-10-22 20:28:03 -0600

    code finished and ready to deploy

--- index.js
+++ index.js
@@ -1,15 +1,75 @@
-//Get Candidate Github list
-//-setup query
-//-put url together (FormatQueryParams)
-//-add API key to header
-//-FETCH(url, options)
-// .then
-// .then
-// .catch
+const GITHUB_URL = "https://api.github.com/";
+const A1 = "c085c9981358";
+const A2 = "e574cdf9e187acf8";
+const A3 = "c552d72d6db7";
+const API_KEY = A1 + A3 + A2;
 
-//Display results
+// Get Candidate Github list
+function getGithubList(userHandle) { 
+  // -setup query
+  const params = {
+    type: "owner",
+    sort: "updated",
+    direction: "desc",
+  };
+  // -put url together (FormatQueryParams)
+  const queryString = formatQueryParams(params);
+  const url = `${GITHUB_URL}users/${userHandle}/repos?${queryString}`;
+  // -add API key to header
+  const options = {
+    headers: new Headers({
+      "X-GitHub-OTP": API_KEY})
+  };
+  // -FETCH
+  fetch(url, options)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(err => {
+      $('.js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+}
 
-//Watch for event submit on button
-//(send info to function)
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+  .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
+  return queryItems.join('&');
+}
+
+// Display results
+function displayResults(responseJson) {
+  console.log(responseJson);
+  $('.js-results-list').empty();
+
+  for (let obj in responseJson) {
+    $('.js-results-list').append(
+      `<li><h3><a href="${responseJson[obj].html_url}">${responseJson[obj].name}</a></h3>
+      <p>Created: ${responseJson[obj].created_at.substr(0, responseJson[obj].created_at.indexOf('T'))}</p>
+      <p>By ${responseJson[obj].owner.login}</p>
+      <p>Description: ${responseJson[obj].description}</p>
+      <p>Language: ${responseJson[obj].language}</p>
+      </li>`
+    );
+  }
+
+  $('.js-results').removeClass('hidden');
+}
+
+// Watch for event submit on button
+// (send info to function)
+function watchForm() {
+  $('.js-form').submit(event => {
+    event.preventDefault();
+    const userHandle = $('.js-user-handle').val();
+
+    $('.js-user-handle').val('');
+    getGithubList(userHandle);
+  });
+}
 
 // Jquery watch form
+$(watchForm);
